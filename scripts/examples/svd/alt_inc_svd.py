253facade294869890761d8b1dbf9f10c6cb64fd
Arya Prasetya <arya.prasetya1997@gmail.com>
2025-07-11 00:50:00 +0200

    altinc

--- scripts/examples/svd/alt_inc_svd.py
+++ scripts/examples/svd/alt_inc_svd.py
@@ -9,23 +9,26 @@ rng = mersenne_twister(42)
 # Reference
 from hasvd.utils.svd import svd_with_tol
 
-tol = 1e-5
+tol = 1e-6
 
-A = random_hankel(2000, 2000, rng, "lrf", 400)
+A = random_hankel(5000, 5000, rng, "lrf", 50)
+
+# Alternating incremental tree
+from hasvd.utils.trees import (
+    regular_alt_inc_tree,
+    regular_alt_inc_general_ltb_map,
+    tlbd_dist_hasvd_tree,
+    tlbd_general_ltb_map,
+)
 
 print(np.linalg.matrix_rank(A))
 
-U, S, Vt = svd_with_tol(A, truncate_tol=tol)
+d_lengths = [100, 900, 1000, 2000, 3000, 3000]  # Prepare diagonal sizes
+outer_dir = 1
 
-# Alternating incremental tree
-from hasvd.utils.trees import alt_inc_tree, alt_inc_general_ltb_map
+tree = regular_alt_inc_tree(500, 5000, outer_dir, "hankel")
 
-d_lengths = [200] * 10  # Prepare diagonal sizes
-outer_dir = 0
-
-tree = alt_inc_tree(d_lengths, outer_dir)
-
-ltb_map = alt_inc_general_ltb_map(A, d_lengths, outer_dir)
+ltb_map = regular_alt_inc_general_ltb_map(A, 500, outer_dir)
 
 # Ascribe nodal error and omega
 from hasvd.utils.errors import tight_error
@@ -35,16 +38,30 @@ omega = 0.1
 
 nodal_error = lambda node: tight_error(node, tol, omega, branch_node_count(tree))
 
-# HASVD
-from hasvd.utils.svd import hasvd
+# HASVD Alt inc
+from hasvd.utils.svd import hasvd, symmetric_cache_map, simple_cache_map
+from time import perf_counter
 
-Uh, Sh, Vth = hasvd(tree, ltb_map, nodal_error)
+print("HASVD Alternating Incremental")
+start = perf_counter()
+Uh, Sh, Vth = hasvd(tree, ltb_map, nodal_error, cache_map=symmetric_cache_map)
+duration = perf_counter() - start
+print("Error:", np.linalg.norm(A - Uh @ np.diag(Sh) @ Vth))
+print("Runtime (pruned):", duration)
 
-print(np.linalg.norm(A - Uh @ np.diag(Sh) @ Vth))
+# HASVD Dist
 
-import matplotlib.pyplot as plt
+tree = tlbd_dist_hasvd_tree(10, 10, outer_dir, (500, 500), recursion="hankel")
 
-plt.semilogy(Sh)
-plt.semilogy(S)
+ltb_map = tlbd_general_ltb_map(A, 10, 10, 500, 500, outer_dir)
+
+nodal_error = lambda node: tight_error(node, tol, omega, branch_node_count(tree))
+
+print("HASVD Two-Level Distributed")
+start = perf_counter()
+Uh, Sh, Vth = hasvd(tree, ltb_map, nodal_error, cache_map=simple_cache_map)
+duration = perf_counter() - start
+print("Error:", np.linalg.norm(A - Uh @ np.diag(Sh) @ Vth))
+print("Runtime (pruned):", duration)
 
 # %%
